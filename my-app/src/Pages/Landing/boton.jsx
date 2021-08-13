445c0ddbba9453bf5ffb039d2b7f468b096c5fdb
isisalejos <75032385+isisalejos@users.noreply.github.com>
2021-08-12 21:30:38 -0400

    boton y vientos
    componentes

---
+++ my-app/src/Pages/Landing/boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const boton = () => {
+    return (
+        <div>
+            <button className="bg-black rounded" onClick={}>Ver +</button>
+        </div>
+    )
+}
+
+export default boton
+
