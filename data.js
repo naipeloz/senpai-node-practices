747ea22f7efe40f00b59c73630c40d8a9c3d70fa
Federico <federico@monkis.uy>
2021-08-12 21:50:32 -0300

    ahi va

---
+++ data.js
@@ -0,0 +1,12 @@
+const ciudades = [
+        {id: 1, name:'Montevideo'},
+        {id: 2, name:'Canelones'},
+        {id: 3, name:'Maldonado'},
+        {id: 4, name:'Minas'},
+        {id: 5, name:'San José'},
+        {id: 6, name:'Florida'},
+    ]
+
+module.exports = {
+    ciudades
+};
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const data = require('./data');
+const app = express();
+
+
+
+app.use(express.json());
+
+app.get('/', (request, response)=>{
+    response.responseStatus = 200;
+    response.send('Welcome Grupo 2');
+});
+
+app.get('/ciudades',(req, res)=>{
+    // console.log(data.ciudades);   
+    res.json(data.ciudades)     
+});
+
+// //ESTA ES LA MEJOR FORMA DE TRAR PARAMETROS
+// app.get('/test/:name',(req, res)=>{
+//     const nombre = req.params.name;
+//     res.send(`Este es el nombre, ${nombre} ?`);
+// })
+// //ACEPTA TODAS LAS LLAMADAS
+// app.all('/myPost/:name',(req, res)=>{
+//     // ESTO VIENE EN EL GET
+//     const misParametros = req.params;
+//     console.log(misParametros);
+//     //ESTO VIENE EN EL BODY
+//     const myBody  = req.body;
+//     console.log(myBody);
+//     // res.send(`Este es el nombre, ${misParametros.name}  <br > ${myBody}`);
+//     // res.send(`<br /> ${myBody.nombre} ${myBody.age} ${myBody.Color}`);
+//     res.json(myBody);
+// })
+
+
+
+app.listen(80);
