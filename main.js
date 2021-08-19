ad7b2a4c5cc9031bee6c5058f10f6e539fc0087d
Julian Salcedo <julianpeloz@gmail.com>
2021-08-19 20:07:10 -0300

    fixing heroku

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 const userRoutes = require('./routes/user');
 
-app.get('/', (resquest, response) => {
+app.get('/', (request, response) => {
   response.send("Bienvenidos a nuestra app");
 })
 
