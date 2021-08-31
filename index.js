c6eb1a99967277acb00ca9f649e088b5f8265cfa
naipeloz <julianpeloz@gmail.com>
2021-08-31 20:18:35 -0300

    Updating root

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
+// Import libs
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Setup app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use((request,response, next) => {
-  console.log('Estoy en el middleware');
-  next();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// CORS 
+app.use(cors());
+
+// Routes
+app.get('/', (response, request) => {
+  request.send("Practica Senpai");
 })
 
-app.get('/', (request, response) => {
-  try{
-    myError.name = 100;
-  } catch (err) {
-    response.send("Ha ocurrido un error, intentalo de nuevo");
-    console.log("Error: ", err);
-  }
-})
-
-app.get('/error', (request, response) => {
-  setTimeout(() => {
-    try{
-      myError.name = 'Borojo';
-    } catch (err) {
-      response.send("Ha ocurrido un error, intentalo de nuevo");
-      console.log("Error: ", err);
-    }
-  }, 6000);
-})
-
+// Run server
 app.listen(PORT, () => {
   console.log('server working');
 });
