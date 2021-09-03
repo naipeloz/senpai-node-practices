4cd81088ba10394644be654b1cf6349c249be401
isisalejos <75032385+isisalejos@users.noreply.github.com>
2021-09-02 21:44:19 -0300

    miau

--- routes/groupTwo.js
+++ routes/groupTwo.js
@@ -7,4 +7,15 @@ router.get('/', (request, response) => {
   response.render('groupTwo/index');
 })
 
+router.post('/consult-city-tem', async (request, response) => {
+  const city = request.body.city;
+  const day =request.body.day;
+  const temperature = request.body.temperature;
+})
+
+router.get('/city-tem', (request, response) => {
+  response.render('groupTwo/index');
+})
+
+
 module.exports = router;
